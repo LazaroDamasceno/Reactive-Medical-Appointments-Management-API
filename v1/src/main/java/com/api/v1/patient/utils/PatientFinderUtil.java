8abc4a5241d6772eb5d2814252a12e392f668046
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-17 03:14:39 -0300

    Improved PatientFinderUtil

--- v1/src/main/java/com/api/v1/patient/utils/PatientFinderUtil.java
+++ v1/src/main/java/com/api/v1/patient/utils/PatientFinderUtil.java
@@ -1,8 +1,10 @@
 package com.api.v1.patient.utils;
 
+import com.api.v1.patient.PatientNotFoundException;
 import com.api.v1.patient.domain.Patient;
 import com.api.v1.patient.domain.PatientRepository;
 import com.api.v1.user.domain.User;
+import com.api.v1.user.domain.UserRepository;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,8 +16,18 @@ public class PatientFinderUtil {
     @Autowired
     private PatientRepository repository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public Mono<Patient> find(@NotNull User user) {
-        return repository.findByPatient(user);
+        return userRepository
+                .findBySsn(user.getSsn())
+                .hasElement()
+                .flatMap(exists -> {
+                    if (exists) return repository.findByPatient(user);
+                    else return Mono.error(new PatientNotFoundException(user.getSsn()));
+                });
+
     }
 
 }
