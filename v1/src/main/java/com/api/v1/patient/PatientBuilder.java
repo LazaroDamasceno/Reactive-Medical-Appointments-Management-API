8878cf856164ba9f2fde496ee776df3ee1202853
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-17 00:25:19 -0300

    Created the builder PatientBuilder

---
+++ v1/src/main/java/com/api/v1/patient/PatientBuilder.java
@@ -0,0 +1,30 @@
+package com.api.v1.patient;
+
+import com.api.v1.patient.domain.Patient;
+import com.api.v1.user.domain.User;
+import com.api.v1.user.dtos.NewUserRequestDto;
+import jakarta.validation.Valid;
+
+import java.util.UUID;
+
+public class PatientBuilder {
+
+    private final UUID id = UUID.randomUUID();
+    private String address;
+    private User user;
+
+    public PatientBuilder withAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public PatientBuilder withUser(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public Patient build() {
+        return new Patient(id, address, user);
+    }
+
+}
