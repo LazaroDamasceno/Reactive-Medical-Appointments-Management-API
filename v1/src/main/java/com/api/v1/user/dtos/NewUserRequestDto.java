adcf05be3752312b6343ffc0d5c333ba49bd0379
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-16 23:26:12 -0300

    Updated NewUserRequestDto

---
+++ v1/src/main/java/com/api/v1/user/dtos/NewUserRequestDto.java
@@ -0,0 +1,22 @@
+package com.api.v1.user.dtos;
+
+import com.api.v1.user.annotations.Gender;
+import com.api.v1.user.annotations.PhoneNumber;
+import com.api.v1.user.annotations.SSN;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record NewUserRequestDto(
+        @NotBlank String firstName,
+        @NotBlank String middleName,
+        @NotBlank String lastName,
+        @NotNull LocalDate birthDate,
+        @SSN String ssn,
+        @Email @NotBlank String email,
+        @PhoneNumber String phoneNumber,
+        @Gender String gender
+) {
+}
