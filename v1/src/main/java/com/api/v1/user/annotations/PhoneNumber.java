36c6f76d07b8e162947154738f40e7a79593ec16
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-16 22:28:27 -0300

    Created the annotations PhoneNumber and SSN

---
+++ v1/src/main/java/com/api/v1/user/annotations/PhoneNumber.java
@@ -0,0 +1,15 @@
+package com.api.v1.user.annotations;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@NotNull
+@Size(min = 10, max = 10)
+public @interface PhoneNumber {}
