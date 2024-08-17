7b86e542944f04aaa97f20355896aa07ecda8b48
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-16 22:34:11 -0300

    Updated dto UserRequestDto

--- v1/src/main/java/com/api/v1/user/dtos/UserRequestDto.java
+++ v1/src/main/java/com/api/v1/user/dtos/UserRequestDto.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public record UserRequestDto(
-        UUID id,
         String firstName,
         String middleName,
         String lastName,
