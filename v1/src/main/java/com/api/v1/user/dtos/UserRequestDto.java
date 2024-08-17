5aaee0133945feea28b3986a11d5f345446acd0e
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-16 22:33:26 -0300

    Created the dto UserRequestDto

---
+++ v1/src/main/java/com/api/v1/user/dtos/UserRequestDto.java
@@ -0,0 +1,17 @@
+package com.api.v1.user.dtos;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record UserRequestDto(
+        UUID id,
+        String firstName,
+        String middleName,
+        String lastName,
+        LocalDate birthDate,
+        String ssn,
+        String email,
+        String phoneNumber,
+        String gender
+) {
+}
