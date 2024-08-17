da2a1c8d8d6f995c27ff4a42a82f78bc92b72d82
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-17 02:53:49 -0300

    Fixed PatientRepository

--- v1/src/main/java/com/api/v1/patient/domain/PatientRepository.java
+++ v1/src/main/java/com/api/v1/patient/domain/PatientRepository.java
@@ -2,6 +2,7 @@ package com.api.v1.patient.domain;
 
 import com.api.v1.user.domain.User;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 public interface PatientRepository extends ReactiveCrudRepository<Patient, UUID> {
 
+    @Query("{ 'user': {$eq user} }")
     Mono<Patient> findByPatient(@NotNull User user);
 
 }
