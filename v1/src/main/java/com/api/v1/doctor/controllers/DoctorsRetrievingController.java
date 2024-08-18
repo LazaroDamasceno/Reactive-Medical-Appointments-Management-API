62496478610e161c086544eccb2ba795d9ceacbf
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-18 00:59:30 -0300

    Created the controller DoctorsRetrievingController

---
+++ v1/src/main/java/com/api/v1/doctor/controllers/DoctorsRetrievingController.java
@@ -0,0 +1,26 @@
+package com.api.v1.doctor.controllers;
+
+import com.api.v1.doctor.domain.Doctor;
+import com.api.v1.doctor.services.DoctorsRetrievingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("api/v1/doctors")
+@RequiredArgsConstructor
+public class DoctorsRetrievingController {
+
+    private final DoctorsRetrievingService service;
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<Doctor> findAll() {
+        return service.findAll();
+    }
+
+}
