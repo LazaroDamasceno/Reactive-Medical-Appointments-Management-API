ffd52993b0532ae8b591eae928939e9fca587913
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-17 23:48:29 -0300

    Created the util DoctorFinderUtil

---
+++ v1/src/main/java/com/api/v1/doctor/utils/DoctorFinderUtil.java
@@ -0,0 +1,23 @@
+package com.api.v1.doctor.utils;
+
+import com.api.v1.doctor.annotations.DLN;
+import com.api.v1.doctor.domain.Doctor;
+import com.api.v1.doctor.domain.DoctorRepository;
+import com.api.v1.doctor.exceptions.DoctorNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class DoctorFinderUtil {
+
+    private final DoctorRepository doctorRepository;
+
+    public Mono<Doctor> find(@DLN String doctorLicenseNumber) {
+        return doctorRepository
+                .findByLicenseNumber(doctorLicenseNumber)
+                .switchIfEmpty(Mono.error(DoctorNotFoundException::new));
+    }
+
+}
