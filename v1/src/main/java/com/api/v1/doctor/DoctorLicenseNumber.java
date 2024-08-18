079197a2cd738adc20cede12cdf50b80ba2fd678
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-17 21:49:18 -0300

    Created the annotation DoctorLicenseNumber

---
+++ v1/src/main/java/com/api/v1/doctor/DoctorLicenseNumber.java
@@ -0,0 +1,15 @@
+package com.api.v1.doctor;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@NotNull
+@Size(min = 7, max = 7)
+public @interface DoctorLicenseNumber {}
