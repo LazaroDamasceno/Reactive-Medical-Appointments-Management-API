2fcd0cc6b3bf08779024fb872257a40878171672
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-18 22:18:10 -0300

    Created the dto MedicalAppointmentResponseDto

---
+++ v1/src/main/java/com/api/v1/medicalappointment/dtos/MedicalAppointmentResponseDto.java
@@ -0,0 +1,14 @@
+package com.api.v1.medicalappointment.dtos;
+
+import com.api.v1.doctor.dtos.DoctorResponseDto;
+import com.api.v1.patient.dtos.PatientResponseDto;
+
+public record MedicalAppointmentResponseDto(
+        DoctorResponseDto doctor,
+        PatientResponseDto patient,
+        String bookingDate,
+        String cancellationDate,
+        String finishingDate,
+        String medicalNote
+) {
+}
