8558a0878e75d9b7d17abefc4e9690a55dd36c30
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-18 22:20:42 -0300

    Created the mapper MedicalAppointmentResponseMapper

---
+++ v1/src/main/java/com/api/v1/medicalappointment/MedicalAppointmentResponseMapper.java
@@ -0,0 +1,21 @@
+package com.api.v1.medicalappointment;
+
+import com.api.v1.doctor.mappers.DoctorResponseMapper;
+import com.api.v1.medicalappointment.domain.MedicalAppointment;
+import com.api.v1.medicalappointment.dtos.MedicalAppointmentResponseDto;
+import com.api.v1.patient.mapper.PatientResponseMapper;
+
+public class MedicalAppointmentResponseMapper {
+
+    public static MedicalAppointmentResponseDto map(MedicalAppointment appointment) {
+        return new MedicalAppointmentResponseDto(
+                DoctorResponseMapper.map(appointment.getDoctor()),
+                PatientResponseMapper.map(appointment.getPatient()),
+                appointment.getBookingDate(),
+                appointment.getCancellationDate(),
+                appointment.getFinishingDate(),
+                appointment.getMedicalNote()
+        );
+    }
+
+}
