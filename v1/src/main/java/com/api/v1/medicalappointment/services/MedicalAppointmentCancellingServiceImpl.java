57d386ceb936a86ffd94dbb8312ccbd3f5e1ad32
LazaroDamasceno <lazarolawrence2@gmail.com>
2024-08-19 16:50:38 -0300

    Updated MedicalAppointmentCancellingServiceImpl

--- v1/src/main/java/com/api/v1/medicalappointment/services/MedicalAppointmentCancellingServiceImpl.java
+++ v1/src/main/java/com/api/v1/medicalappointment/services/MedicalAppointmentCancellingServiceImpl.java
@@ -5,6 +5,7 @@ import com.api.v1.doctor.utils.DoctorFinderUtil;
 import com.api.v1.medicalappointment.domain.MedicalAppointment;
 import com.api.v1.medicalappointment.domain.MedicalAppointmentRepository;
 import com.api.v1.medicalappointment.dtos.MedicalAppointmentDataRequestDto;
+import com.api.v1.medicalappointment.exceptions.MedicalAppointmentAlreadyCancelledException;
 import com.api.v1.medicalappointment.utils.MedicalAppointmentFinderUtil;
 import com.api.v1.patient.domain.Patient;
 import com.api.v1.patient.utils.PatientFinderUtil;
@@ -33,6 +34,8 @@ class MedicalAppointmentCancellingServiceImpl implements MedicalAppointmentCance
                     Doctor doctor = tuple.getT1();
                     return appointmentFinderUtil
                             .find(doctor, patient, dto.bookedDate())
+                            .filter(e -> e.getCancellationDate() != null && e.getFinishingDate() == null)
+                            .switchIfEmpty(Mono.error(MedicalAppointmentAlreadyCancelledException::new))
                             .flatMap(e -> {
                                 e.cancelAppointment();
                                 return repository.save(e);
