d411db5a063810b41cd6abab22406905344cc286
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2024-08-21 17:59:16 +0000

    Deleted UserFinderUtil and UserNotFoundException

--- v1/src/main/java/com/api/v1/user/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.api.v1.user.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException() {
-        super("User was not found.");
-    }
-
-}
